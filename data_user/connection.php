d643141da8c2b33b12df49fc704c23f95d748a35
mhabib04 <mhabib180404@gmail.com>
2024-05-08 07:06:36 +0700

    Initial commit

--- data_user/connection.php
+++ data_user/connection.php
@@ -5,20 +5,20 @@ $connection = null;
 try{
     //Config
     $host = "localhost";
-    $username = "root";
-    $password = "";
-    $dbname = "data_user";
+    $username = "bibskymy_user";
+    $password = "mhabib1804";
+    $dbname = "bibskymy_data_user";
 
     //Connect
     $database = "mysql:dbname=$dbname;host=$host";
     $connection = new PDO($database, $username, $password);
     $connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-    // if($connection){
-    //     echo "Koneksi Berhasil";
-    // } else {
-    //     echo "Gagal gan";
-    // }
+    if($connection){
+        echo "Koneksi Berhasil";
+    } else {
+        echo "Gagal gan";
+    }
 
 
 } catch (PDOException $e){
